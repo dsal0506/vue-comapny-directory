0cbee9cb829b4bfdb2436d72777d4c9bd0335b80
dsal0506 <dsalinas_5605@stu.southtexascollege.edu>
2023-09-12 13:45:17 -0500

    added nav layout

---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<script setup>
+  import { ref } from 'vue'
+  const brand = ref('🏫 Fake Comanpany Directory')
+</script>
+
+<template>
+  <nav>
+      <div class="wrapper">
+        <div class="brand">
+        <span class="brand-title">{{ brand }}</span>
+      </div>
+      <div class="menu">
+        <a href="#" clas="menu-item">Department</a>
+        <a href="#" clas="menu-item">Setings</a>
+        <a href="#" clas="menu-item">Logout</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="postcss">
+  nav {
+    @apply flex h-20  bg-slate-900 text-slate-200;
+    .wrapper {
+      @apply container mx-auto flex w-full items-center justify-between;
+      .brand{
+        &-title {
+          @apply text-2xl font-bold text-yellow-500;
+        }
+      }
+      .menu{
+        @apply flex gap-2;
+        &-item {
+          @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
+        }
+        &-login {
+          @apply rounded-md bg-red-500 px-4 py-2 text-red-100 hover:text-red-700;
+        }
+      }
+    }
+  }
+</style>
